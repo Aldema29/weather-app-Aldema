d12404f1800c26b79f1f5049af99346491cfbd29
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-12 21:50:29 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ function displayTemperature(response) {
     let cityElement = document.querySelector("#city");
    temperatureElement.innerHTML = Math.round
    (response.data.main.temp);
-  cityElement
+  cityElement.innerHTML = response.data.name;
+}
 
   function searchCity(city) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(showTemperature);
+    axios.get(apiUrl).then(displayTemperature);
   }
-    
