23e6bec72b534ddb1308a4e5da704c2c561c313b
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-12 19:40:10 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 let apiKey = "4805d215b0d139399d8997a4c097e47c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?g=Tel aviv&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?g=Tel Aviv&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl):
+
+axios.get(url).then(displayTemperature);
