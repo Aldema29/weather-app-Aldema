b1ef0cd73e3a6b04edb2868f3cb675c768d7892d
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-12 20:37:18 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-function displayTemperature(response){
-    console.log(response.data);
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = response.data.main.temp;
+function showUser(response) {
+    alert(`The user name is ${response.data.name}`);
 }
 
 
 let apiKey = "6210bfb6041b002d1b53875oa36td949";
-let apiUrl = "https://api.shecodes.io/weather/v1/current?query=Tel-aviv&key=6210bfb6041b002d1b53875oa36td949&units=metric"
+let url = "https://api.shecodes.io/weather/v1/current?query=Tel-aviv&key=6210bfb6041b002d1b53875oa36td949&units=metric"
 
-axios.get(apiUrl).then(displayTemperature);
+
+
+  let url = "https://jsonplaceholder.typicode.com/users/1";
+  axios.get(url).then(showUser);
