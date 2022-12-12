702bb05f75e1827b486da32145b26a9766c1eb82
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-12 21:31:24 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,13 @@
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#humidity");
-    let windElement = document.querySelector("#wind");
-    let dateElement = document.querySelector("#date");
-    let iconElement = document.querySelector("#icon");
+   temperatureElement.innerHTML = Math.round
+   (response.data.main.temp);
+  cityElement
 
     function search(city) {
         let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
         let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then(displayTemperature);
       }
-      
-      function handleSubmit(event) {
-        event.preventDefault();
-        let cityInputElement = document.querySelector("#city-input");
-        search(cityInputElement.value);
-      }
-      
-      let form = document.querySelector("#search-form");
-      form.addEventListener("submit", handleSubmit);
-      
-      search("Tel-Aviv");
+    
