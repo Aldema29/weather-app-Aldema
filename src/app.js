f888afc5fb73d1583d4436d7412afb6bd2e7c18b
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-12 20:48:09 +0200

    updates JS

--- src/app.js
+++ src/app.js
@@ -7,57 +7,19 @@ function displayTemperature(response) {
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
 
-celsiusTemperature = response.data.main.temp;
-
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-    cityElement.innerHTML = response.data.name;
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
-    iconElement.setAttribute("alt",response.data.weather[0].description)
-}
-
-function search(city) {
-    let apiKey = "fb53e9f0cdf1fede8235fdb56573a63e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
-}
-
-function handleSubmit(event){
-    event.preventDefault();
-    let cityInputElement = document.querySelector("#city-input");
-    search(cityInputElement.value);
-}
-
-function convertToFahrenheit(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    let fahrenheitTemperature = (celsiusTemperature * 9)/5 + 32;
-    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-  }
-  
-  function convertToCelsius(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  }
-
-  let celsiusTemperature = null;
-
-  let form= document.querySelector("#search-form");
-  form.addEventListener("submit",handleSubmit);
-  
-  let fahrenheitLink = document.querySelector("#fahrenheit-link");
-  fahrenheitLink.addEventListener("click", convertToFahrenheit);
-  
-  let celsiusLink = document.querySelector("#celsius-link");
-  celsiusLink.addEventListener("click", convertToCelsius);
-
-  search("Perth");
+    function search(city) {
+        let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(displayTemperature);
+      }
+      
+      function handleSubmit(event) {
+        event.preventDefault();
+        let cityInputElement = document.querySelector("#city-input");
+        search(cityInputElement.value);
+      }
+      
+      let form = document.querySelector("#search-form");
+      form.addEventListener("submit", handleSubmit);
+      
+      search("Tel-Aviv");
