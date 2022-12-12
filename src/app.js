a48ff711d28a1e30e0d186d8f52aebe0fde85839
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-12 20:29:34 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,6 @@ function displayTemperature(response){
 
 
 let apiKey = "6210bfb6041b002d1b53875oa36td949";
-let apiUrl = "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=6210bfb6041b002d1b53875oa36td949&units=metric"
+let apiUrl = "https://api.shecodes.io/weather/v1/current?query=Tel-aviv&key=6210bfb6041b002d1b53875oa36td949&units=metric"
 
 axios.get(apiUrl).then(displayTemperature);
