6da04f5d71f6acef6d3cdfbdd6ba2bfcbb194826
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-12 19:33:36 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,4 @@
 let apiKey = "4805d215b0d139399d8997a4c097e47c";
-let apiUrl = "https://smaples.openweathermap.org/data/2.5/weather?g=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?g=Tel aviv&appid=${apiKey}`;
+
+console.log(apiUrl):
