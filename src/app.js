b7a0d7ad15b3bd99c00744140bf6461afc46d202
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-12 20:26:50 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 function displayTemperature(response){
     console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = response.data.main.temp;
 }
+
 
 let apiKey = "6210bfb6041b002d1b53875oa36td949";
 let apiUrl = "https://api.shecodes.io/weather/v1/current?query=Tel Aviv&key=6210bfb6041b002d1b53875oa36td949&units=metric"
