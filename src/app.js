1c1eccf431bbc75715cc61f0c4646e368672f6ad
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-13 18:08:48 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ function displayTemperature(response) {
     let humdityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
@@ -12,7 +13,12 @@ function displayTemperature(response) {
     humdityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.main.wind);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/04d@2x.png`
+        );
 }
+
 
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let city = "Tel aviv";
