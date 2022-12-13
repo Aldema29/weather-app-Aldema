aebf85a3c59bd13231e9a0b0f2ca832d2883a90a
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-13 17:53:24 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,15 +4,18 @@ function displayTemperature(response) {
     let descriptionElement = document.querySelector("#description");
     let humdityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humdityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.main.wind);
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-  function searchCity(city) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let city = "Tel aviv";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    
     axios.get(apiUrl).then(displayTemperature);
-  }
