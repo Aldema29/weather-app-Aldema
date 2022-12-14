d7f21df37068c1cec1a8847d633f1c51423ea1d4
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-14 18:42:08 +0200

    some update to js and html

--- src/app.js
+++ src/app.js
@@ -4,14 +4,13 @@ let hours = date.getHours();
 if (hours < 10) {
     hours = `0${hours}`;
 }
-
 let minutes = date .getMinutes();
 if (minutes < 10) {
     minutes = `0${minutes}`;
 }
 
 let days = [
-"sunday", 
+"Sunday", 
 "Monday", 
 "Tuesday", 
 "Wednesday", 
@@ -35,7 +34,7 @@ function displayTemperature(response) {
 
     celsiusTemperature = response.data.main.temp;
 
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
