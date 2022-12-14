53653423905c0f07adbf4247cc85812f52413276
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-14 17:43:44 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
-    let humdityElement = document.querySelector("#humidity");
+    let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
@@ -34,7 +34,7 @@ function displayTemperature(response) {
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
-    humdityElement.innerHTML = response.data.main.humidity;
+    humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.main.wind);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute(
