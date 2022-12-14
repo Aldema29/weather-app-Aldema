8c46c9289d4c23e70fe6449764f6621490cb4d5f
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-14 15:15:27 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -46,7 +46,6 @@ function displayTemperature(response) {
 
 function search(city){
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let city = "Tel aviv";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
 }
