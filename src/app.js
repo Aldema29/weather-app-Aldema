479337db4e976a56799ab3d9e9016e047a61a1f9
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-14 18:06:07 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ let hours = date.getHours();
 if (hours < 10) {
     hours = `0${hours}`;
 }
+
 let minutes = date .getMinutes();
 if (minutes < 10) {
     minutes = `0${minutes}`;
 }
+
 let days = [
 "sunday", 
 "Monday", 
