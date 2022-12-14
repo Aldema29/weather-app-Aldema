db6bd65ad0b9d9ddfaf27412f88680b67bfe2048
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-14 13:50:07 +0200

    making the search bar work via API

--- src/app.js
+++ src/app.js
@@ -43,8 +43,23 @@ function displayTemperature(response) {
         );
     iconElement.setAttribute("alt", response.data.weather[0].description);
    }
+
+function search(city){
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let city = "Tel aviv";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    
     axios.get(apiUrl).then(displayTemperature);
+}
+
+
+   function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+
+   }
+
+
+
+   let form = document.querySelector("search-from");
+   form.addEventListener("submit", handleSubmit);
+
