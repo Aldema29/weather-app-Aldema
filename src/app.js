05770dfcc988a898b2485007e5bcde49ee63fea4
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-18 23:18:35 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ return `${day} ${hours}:${minutes}`;
 function displayForecast() {
     let forecastElement = document.querySelector("#forecast");
   
-    let days = ["Thu", "Fri", "Sat", "Sun" , "Mon" , "Tue" , "Wed"];
+    let days = ["Mon", "Tue", "Wed", "Thur" , "Fri" , "Sat" , "Sun"];
   
     let forecastHTML = `<div class="row">`;
     days.forEach(function (day) {
