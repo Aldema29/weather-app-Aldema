ef429524436305fd54755d94a4db16c60dbbb295
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-19 10:32:08 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ function displayTemperature(response){
     let dateElement = document.querySelector("#date")
     let iconElement = document.querySelector("#icon")
 
-    celsiusTemperature = response.data.temperature.current;
+    celsiusTemperature = response.data.main.current;
 
    temperatureElement.innerHTML = Math.round(celsiusTemperature);
    cityElement.innerHTML = response.data.city;
