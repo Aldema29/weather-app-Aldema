d5305cf74ed3d420eae1dba943e8275e7d01f6b3
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-19 09:58:13 +0200

    updated everything

--- src/app.js
+++ src/app.js
@@ -1,145 +1,104 @@
-function formatDate(timestamp) {
-let date = new Date(timestamp);
-let hours = date.getHours();
-if (hours < 10) {
-    hours = `0${hours}`;
-}
-let minutes = date .getMinutes();
-if (minutes < 10) {
-    minutes = `0${minutes}`;
-}
-
-let days = [
-"Sunday", 
-"Monday", 
-"Tuesday", 
-"Wednesday", 
-"Thursday", 
-"Friday", 
-"Saturday",
-];
-let day = days[date.getDay()];
-return `${day} ${hours}:${minutes}`;
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours} : ${minutes}`;
 }
 
-function formatDay(timestamp) {
-    let date = new Date(timestamp * 1000);
-    let day = date.getDay();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  
-    return days[day];
-  }
+function formatDay(timestamp){
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-function displayForecast(response) {
+return days[day]; 
+}
+
+function displayForecast(response){
     let forecast = response.data.daily;
-
-    let forecastElement = document.querySelector("#forecast");
    
+let forecastElement = document.querySelector("#forecast");
+
     let forecastHTML = `<div class="row">`;
-    forecast.forEach(function (forecastDay, index) {
-        if (index < 6) {
-      forecastHTML =
-        forecastHTML +
-        `
+    
+   forecast.forEach(function (forecastDay, index) {
+       if (index < 6){   
+        forecastHTML = 
+        forecastHTML + 
+        ` 
         <div class="col-2">
-          <div class="weather-forecast-date">${formatDay(
-          forecastDay.dt
-          )}</div>
-          <img
-            src="http://openweathermap.org/img/wn/${
-                forecastDay.weather[0].icon
-              }@2x.png"
-              alt=""
-              width="42"
-          />
+          <div class="weather-forecast-date">${formatDay(forecastDay.time * 1000)}</div>
+          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png" 
+          alt=""
+          width="42"/>
           <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"> ${Math.round(
-                forecastDay.temp.max
-            )}°</span>
-            <span class="weather-forecast-temperature-min"> ${Math.round(
-                forecastDay.temp.min
-            )}° </span>
+          <span class="weather-forecast-temperatures-max"> ${Math.round(forecastDay.temperature.maximum)}° </
+          span>
+            <span class="weather-forecast-temperatures-min">
+             ${Math.round(forecastDay.temperature.minimum)}°
+            </span>
           </div>
         </div>
-    `;
+      `;
       }
     });
-  
-    forecastHTML = forecastHTML + `</div>`;
-    forecastElement.innerHTML = forecastHTML;
-  }
-  
-  function getForecast(coordinates) {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayForecast);
-  }
+        forecastHTML = forecastHTML + `</div`;   
+        forecastElement.innerHTML = forecastHTML
+}
+        function getForecast(coordinates){
+            console.log(coordinates);
+            let apiKey = "f4a7c4a51b002t628840b9bo374f490d"
+            let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${coordinates.latitude}&lon=${coordinates.longitude}&key=${apiKey}&units=imperial`
+            axios.get(apiUrl).then(displayForecast);
+        }
 
-function displayTemperature(response) {
+function displayTemperature(response){
+ 
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
-    let windElement = document.querySelector("#wind");
-    let dateElement = document.querySelector("#date");
-    let iconElement = document.querySelector("#icon");
+    let speedElement = document.querySelector("#speed")
+    let dateElement = document.querySelector("#date")
+    let iconElement = document.querySelector("#icon")
 
-    celsiusTemperature = response.data.main.temp;
+    imperialTemperature = response.data.temperature.current;
 
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-    cityElement.innerHTML = response.data.name;
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.main.wind.speed);
-    dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    iconElement.setAttribute(
-        "src",
-        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-      );
-    iconElement.setAttribute("alt", response.data.weather[0].description);
+    temperatureElement.innerHTML = Math.round(imperialTemperature);
+    cityElement.innerHTML = response.data.city;
+   descriptionElement.innerHTML = response.data.condition.description;
+   humidityElement.innerHTML = response.data.temperature.humidity;
+   speedElement.innerHTML = Math.round(response.data.wind.speed);
+   dateElement.innerHTML = formatDate(response.data.time * 1000);
+   iconElement.setAttribute("src",`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);  
+   iconElement.setAttribute("alt", response.data.condition.description);
 
-    getForecast(response.data.coord);
-   }
+   getForecast(response.data.coordinates);
 
-function search(city) {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayTemperature);
+}
+
+function search(city){
+let apiKey ="f4a7c4a51b002t628840b9bo374f490d";
+let apiUrl = `
+https://api.shecodes.io/weather/v1/current?query=${city}&key=f4a7c4a51b002t628840b9bo374f490d&units=imperial`;  
+axios.get(apiUrl).then(displayTemperature)   
 }
 
 
-   function handleSubmit(event) {
+function handleSubmit(event){
     event.preventDefault();
     let cityInputElement = document.querySelector("#city-input");
-    search(cityInputElement.value);
+    search(cityInputElement.value)
 }
+ 
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
-function displayFahrenheitTemperature(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    let fahrenheitTemperature = (celsiusTemperature * 9) /  5 + 32;
-    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function displayCelsiusTemperature(event) {
-    event.preventDefault();
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-   let form = document.querySelector("#search-form");
-   form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-search("Tel Aviv");
-displayForecast();
+search("Tel-aviv");
