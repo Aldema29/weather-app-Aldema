030aad0ad5927246be646e70d395ee62ff850765
Aldema29 <117029077+Aldema29@users.noreply.github.com>
2022-12-20 20:04:35 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -87,7 +87,7 @@ function displayTemperature(response){
 function search(city){
 let apiKey ="f4a7c4a51b002t628840b9bo374f490d";
 let apiUrl = `
-https://api.shecodes.io/weather/v1/current?query=${city}&key=f4a7c4a51b002t628840b9bo374f490d&units=imperial`;  
+https://api.shecodes.io/weather/v1/current?query=${city}&key=f4a7c4a51b002t628840b9bo374f490d&units=metric`;  
 axios.get(apiUrl).then(displayTemperature)   
 }
 
